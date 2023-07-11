9125144e1517ea5cd707582fc9d0536edabee874
Jose Rafael Torres Calderon <jose.torresc@coppel.com>
2023-07-11 09:38:06 -0700

    Se agregan cambios en plantilla spring boot

--- src/main/java/com/example/demo/Repositories/ClienteRepository.java
+++ src/main/java/com/example/demo/Repositories/ClienteRepository.java
@@ -3,11 +3,11 @@ package com.example.demo.Repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
-
 import com.example.demo.Entity.ClienteEntity;
 
 @Repository
 @EnableJpaRepositories
-public interface ClienteRepository extends JpaRepository<, Integer> {
+// extends JpaRepository<, Integer>
+public interface ClienteRepository {
 
 }
